1135cfef9fba940784241a83b8812e40f1c65928
ycchen <chen042531.cs03@nctu.edu.tw>
2022-01-29 10:24:34 +0000

    fix format

--- gtp5g.c
+++ gtp5g.c
@@ -316,8 +316,8 @@ static char* seid_and_u32id_to_hex_str(u64 seid_int, u32 id)
         GTP5G_ERR(NULL, "Failed to allocate concate id\n");
     }
 
-    snprintf(seid_hexstr, SEID_HEX_STR_LEN ,"%llx",seid_int);
-    snprintf(id_hexstr, U32_ID_HEX_STR_LEN ,"%x",id);
+    snprintf(seid_hexstr, SEID_HEX_STR_LEN, "%llx", seid_int);
+    snprintf(id_hexstr, U32_ID_HEX_STR_LEN, "%x", id);
     strcpy(seid_u32id_hexstr, seid_hexstr);
     strcat(seid_u32id_hexstr, id_hexstr);
 
