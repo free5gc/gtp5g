4bd33469cf9aaafbff377d25630775dce9e2b07d
chen042531 <chen042531@gmail.com>
2024-11-04 17:07:45 +0800

    fix kernel division error (#120)
    * fix kernel division error
    
    * update comment
    
    ---------
    
    Co-authored-by: ycchen <chen042531.cs03@nctu.edu.tw>

--- src/pfcp/urr.c
+++ src/pfcp/urr.c
@@ -267,6 +267,11 @@ struct VolumeMeasurement *get_usage_report_counter(struct urr *urr, bool previou
 {
     u32 now = ktime_get_real() / NSEC_PER_SEC;
 
+    // If the period is zero, always return the first counter.
+    if (urr->period == 0) {
+       return &urr->bytes; 
+    }
+
     if ((now/urr->period)%2 == 1) {
         if (previous_counter) {
             return &urr->bytes;
